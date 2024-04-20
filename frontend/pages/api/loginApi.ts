88725a6cf52fc3000e73e68e2612968a54b8deb2
glen <gjbetonio@ruralnet.ph>
2024-04-21 00:42:58 +0800

    login

---
+++ frontend/pages/api/loginApi.ts
@@ -0,0 +1,38 @@
+
+const apiKey = process.env.NEXT_PUBLIC_API_URL;
+const LoginAPI = {
+    //for log-in users !
+    logIn: async (user_credentials: any) => {
+      try {
+        const response = await fetch(`${apiKey}/user/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user_credentials),
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          const res = 
+          {
+            status : true ,
+            responseData
+          }
+          return res;
+        }else{
+            const res = 
+            {
+              status : false ,
+            }
+          return res;
+
+        }
+        throw new Error(response.status.toString());
+      } catch (error) {
+        throw error;
+      }
+    },
+  };
+  
+  export default LoginAPI;
+  
